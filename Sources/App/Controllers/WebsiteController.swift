e18736c349f954463998cbad9049b6b09be96490
belkhadir <belkhadir.anas@gmail.com>
2019-01-22 18:00:10 +0100

    Feature: rendring the landing Page

--- Sources/App/Controllers/WebsiteController.swift
+++ Sources/App/Controllers/WebsiteController.swift
@@ -2,13 +2,49 @@
 
 import Vapor
 import Leaf
+import Fluent
 
 struct WebsiteController: RouteCollection {
     func boot(router: Router) throws {
         router.get(use: indexHandler)
+        router.get("contact", use: redirectContactMe)
+        router.post(CreatContactMe.self, use: createContackMe)
     }
     
     func indexHandler(_ req: Request) throws -> Future<View> {
-        return try req.view().render("index")
+        let context = IndexContext(title: "News Great Again.")
+        return try req.view().render("index", context)
     }
+    
+    func createContackMe(_ req: Request, data: CreatContactMe) throws ->  Future<HTTPResponse> {
+        guard let email = data.email else {
+            throw Abort(.internalServerError)
+        }
+        
+        guard let fullName = data.fullName else {
+            throw Abort(.internalServerError)
+        }
+        
+        guard let message = data.message else {
+            throw Abort(.internalServerError)
+        }
+        
+        let contact = ContactMe(email: email, fullName: fullName, message: message)
+        
+        return contact.save(on: req).transform(to: HTTPResponse(status: .ok))
+    }
+    
+    func redirectContactMe(_ req: Request) throws -> Future<View> {
+        return try req.view().render("createContactMe")
+    }
+}
+
+struct IndexContext: Encodable {
+    let title: String
+}
+
+struct CreatContactMe: Content {
+    let email: String?
+    let fullName: String?
+    let message: String?
 }
---
+++ Sources/App/Models/ContactMe.swift
@@ -0,0 +1,25 @@
+//
+//  ContactMe.swift
+//  App
+//
+//  Created by Belkhadir Anas on 1/22/19.
+//
+import FluentPostgreSQL
+import Vapor
+import Authentication
+
+final class ContactMe: PostgreSQLModel  {
+    var id: Int?
+    
+    var email: String
+    var fullName: String?
+    var message: String?
+    init(email: String, fullName: String, message: String) {
+        self.email = email
+        self.fullName = fullName
+    }
+}
+
+extension ContactMe: Content {}
+extension ContactMe: Migration {}
+extension ContactMe: Parameter {}
